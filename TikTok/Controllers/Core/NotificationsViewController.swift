e2bbec434958121473e7636a5be2d8753d57ee03
devmatias <devmatias@gmail.com>
2022-03-31 17:57:31 -0300

    Set up tab bar and pods

---
+++ TikTok/Controllers/Core/NotificationsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsViewController.swift
+//  TikTok
+//
+//  Created by Matias Correa Franco de Faria on 31/03/22.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+    }
+}
---
+++ TikTok/Managers/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  TikTok
+//
+//  Created by Matias Correa Franco de Faria on 31/03/22.
+//
+
+import Foundation
